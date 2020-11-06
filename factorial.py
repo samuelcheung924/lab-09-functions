5231c50850564a1bd9de4671042146221bfd8ffd
samuelcheung924 <67924107+samuelcheung924@users.noreply.github.com>
2020-11-06 13:20:34 -0500

    Final Ver

--- factorial.py
+++ factorial.py
@@ -6,23 +6,4 @@ def factorial(n):
     return total
 
 num = int(input("Please enter a number "))
-print(num,"! is ",factorial(num))
-
-
-
-#function factorial(n) {
-#var total = 1;
-#for (i = 0; i < n; i++) {
-#total = total * (n - i);
-#document.write(" Current i is " + i+ "<br>");
-#document.write("Current value of total is " + total+ "<br>");
-#// First time total = 1
-#// incremenet total up to n
-#}
-#return total;
-#// return the final value of total
-#}
-
-#var userstring = prompt("Number please:");
-#var usernum = parseInt(userstring);
-#document.write(usernum + "! is " + factorial(usernum));
+print(f'{num}! is {factorial(num)}')
